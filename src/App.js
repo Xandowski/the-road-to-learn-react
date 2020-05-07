9742677f256257da9fdd53661c10c66f7d00f520
Xandowski <xandy-moraes@hotmail.com>
2020-05-07 13:18:49 -0300

    criando listas e utilizando o metodo map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Welcome To The Road Learn React</h1>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        {list.map((item) => (
+          <div key={item.objectID}>
+            <span>
+              <a href="{item.url}>{item.title}">title</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
