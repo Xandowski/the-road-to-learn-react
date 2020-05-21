a822b9baf1790c33d8a732384bdd2d481970f833
Xandowski <xandy-moraes@hotmail.com>
2020-05-20 22:51:24 -0300

    tratamento de erros

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -56,7 +57,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setSTate({ error }));
   }
 
   componentDidMount() {
@@ -99,7 +100,7 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey} = this.state;
+    const { searchTerm, results, searchKey, error} = this.state;
     const page = (results && results[searchKey] && results[searchKey].page) || 0;
     const list = (
       results &&
@@ -118,14 +119,19 @@ class App extends Component {
             Search
           </Search>
         </div>
-          <Table
-            list={list}
-            onDismiss={this.onDismiss}
-          />
-        <div className="interactions">
-          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
-            More
-          </Button>
+        { error
+          ? <div>
+              <p>Something went wrong.</p>
+            </div>
+          : <Table
+              list={list}
+              onDismiss={this.onDismiss}
+            />
+        }
+          <div className="interactions">
+            <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+              More
+            </Button>
         </div>
       </div>
     );
